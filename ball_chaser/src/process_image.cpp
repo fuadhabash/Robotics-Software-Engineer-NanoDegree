bad6bd575ed5c44bbdc2c417bdf2e1969db0850b
fuadhabash <24967760+fuadhabash@users.noreply.github.com>
2020-05-22 17:03:46 +0200

    Add files via upload

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -39,57 +39,103 @@ void process_image_callback(const sensor_msgs::Image img)
     float x = 0.0;
     float z = 0.0;
     int count = 0;
+    float offset = 0;
 
     bool first_x = false;
-    int center_x;
+    int center_x = 0;
     int position = 0;
+    bool reached = false;
 
     for (int i = 0; i < height ; i++) {
         for (int j = 0; j < step; j++) {
-            if (img.data[i * step + j] == white_pixel) {
+            if (img.data[i * step + j] == white_pixel and img.data[(i * step + j) + 1] == white_pixel and img.data[(i * step + j) + 2] == white_pixel) {
                 if (!first_x) {
                   first_x = true;
-                  center_x = j;
+                  center_x = j+1;
                 }
-                // vec_cout[j]++;
-                //offset += j - step / 2.0;
+                offset += j - img.step / 2.0;   // offset = 0 middle of Image
                 count++;
             }
         }
     }
 
-    if (count == 0) position = 0; // no ball
-    if (center_x < step/3) position = 1; //left
-    if (step/3 <= center_x <= step*2/3) position = 2; //middle
-    if (step*2/3 < center_x) position = 3; //right
-    if (height*step/3 < count) position = 4; //reached
-
-    switch (position) {
-      case 0:
-        x = 0;
-        z = 0;
-        moving_state = false;
-        break;
-      case 1:
-        x = 0;
-        z = -3;
-        moving_state = true;
-        break;
-      case 2:
-        x = 0.1;
-        z = 0;
-        moving_state = true;
-        break;
-      case 3:
-        x = 0;
-        z = 3;
-        moving_state = true;
-        break;
-      case 4:
-        x = 0;
-        z = 0;
-        moving_state = false;
+    if (count == 0) {
+      position = 0; // searching
+      x = 0;
+      z = -1;
     }
+    else if (50000 < count) {
+      position = 4; //reached
+      if(center_x > (step/2 +10)){
+        x = 0;
+        z = 0.2;
+      }
+      if(center_x < (step/2 -10)){
+        x = 0;
+        z = -0.2;
+      }
+      if((step/2 -10) <= center_x <= (step/2 +10)){
+        x = 0;
+        z = 0;
+      }
+      reached = true;
+    }
+    else if (!reached && (center_x < step*3/8)) {
+      position = 1; //left
+      x = 0.2;
+      z = -0.4;
+    }
+    else if (!reached && (step*3/8 < center_x <= step*5/8)) {
+      position = 2; //middle
+      x = 0.3;
+      z = 0;
+    }
+    else if (!reached && (step*5/8 < center_x)) {
+      position = 3; //right
+      x = 0.2;
+      z = 0.4;
+    }
+
+    // if (count == 0) position = 0; // searching
+    // if (center_x < step*3/8) position = 1; //left
+    // if (step*3/8 < center_x <= step*5/8) position = 2; //middle
+    // if (step*5/8 < center_x) position = 3; //right
+    // if (height*step/3 < count) position = 4; //reached
+    // ROS_INFO_STREAM(position);
+    // ROS_INFO_STREAM(center_x);
+    // ROS_INFO_STREAM(count);
+    // ROS_INFO_STREAM(offset);
+
+    // switch (position) {
+    //   case 0:
+    //     x = 0;
+    //     z = 0.8;
+    //     moving_state = true;
+    //     break;
+    //   case 1:
+    //     x = 0.2;
+    //     z = -1;
+    //     moving_state = true;
+    //     break;
+    //   case 2:
+    //     x = 0.1;
+    //     z = 0;
+    //     moving_state = true;
+    //     break;
+    //   case 3:
+    //     x = 0.2;
+    //     z = 1;
+    //     moving_state = true;
+    //     break;
+    //   case 4:
+    //     x = 0;
+    //     z = 0;
+    //     moving_state = false;
+    //     break;
+    //   default:
+    //     x = 0;
+    //     z = 0;
+    // }
 
 
     // Send request to service
